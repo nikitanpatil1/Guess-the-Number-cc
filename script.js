198a0bb7594c69595f2134e94cd99c4e8aa21c1e
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-03-27 14:13:54 +0000

    solution

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
@@ -31,34 +31,79 @@ if(playGame){
 // this function is used to see weather the guess given by user is valid/ not
 // i.e it lies between our condition/not
 function validateGuess(guess){
-
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    }else if(guess < 1){
+        alert('Please enter a number more than 1')
+    }else if(guess > 100){
+        alert('Please enter a number less than 100')
+    }else{
+        prevGuess.push(guess)
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMessage(`Game Over. Random number was ${randomNumber}`)
+            endGame()
+        }else {
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
 }
 
 // used to check the value input by user with random number
 // i.e value > randomNum , value = randomNum , value < randomNum
 function checkGuess(guess){
-
+    if(guess === randomNumber){
+        displayMessage('You guessed it right')
+        endGame()
+    }else if (guess < randomNumber){
+        displayMessage('Number is TOOO low')
+    }else if (guess > randomNumber){
+        displayMessage('Number is TOOO High')
+    }
 }
 
 // will clean the previous values
 // will update the previous guesses and remaining guesses
+// cleanup guess
 function displayGuess(guess){
-
+    userInput.value = ''
+    guessSlot.innerHTML += `${guess}, `
+    numGuess++ ;
+    guessesRemaining.innerHTML = `${11 - numGuess}`
 }
 
 // will display the message of lowOrHigh variable
 function displayMessage(message){
-
+    lowOrHi.innerHTML = `<h2>${message}</h2>`
 }
 
 
 // used to end game
 function endGame(){
-
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame">Start New Game</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
 }
 
 // used to start new game
 function newGame(){
+    const newGameButton = document.querySelector('#newGame')
+    newGameButton.addEventListener('click', function(e){
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = []
+        numGuess = 1
+        guessSlot.innerHTML = ''
+        guessesRemaining.innerHTML = `${11 - numGuess}`
+        userInput.removeAttribute('disabled')
+        startOver.removeChild(p)
+        
+        playGame = true
+    })
 
 }
 
